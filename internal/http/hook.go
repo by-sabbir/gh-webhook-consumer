b7ba7f3fa64c86dfbb44f795eb4687ce031e14c7
by-sabbir <me@sabbir.dev>
2022-11-22 22:13:39 +0600

    feat: issue receiver

--- internal/http/hook.go
+++ internal/http/hook.go
@@ -9,8 +9,9 @@ import (
 )
 
 func (h *Handler) GetHooks(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
 	log.Println("GetHooks")
-	var hook hook.AllEvents
+	var hook hook.GitIssues
 	json.NewDecoder(r.Body).Decode(&hook)
-	json.NewEncoder(w).Encode(hook.Commits)
+	json.NewEncoder(w).Encode(hook)
 }
